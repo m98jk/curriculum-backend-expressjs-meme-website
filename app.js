9563e457727d0a73a608f6f3b6b89e1b6b0de1d0
halitbatur <halitfuatbatur@gmail.com>
2021-10-18 08:55:54 +0300

    clear app.js

--- app.js
+++ app.js
@@ -1,13 +1,1 @@
-const express = require("express");
-
-const bodyParser = require("body-parser");
-const app = express();
-
-app.use(bodyParser.urlencoded({ extended:false }));
-app.set("view engine","ejs");
-app.set("views", "views");
-
-app.get('/',(req,res) => {
-    res.render('index');
-})
-app.listen(3000);
+// Your code goes here
