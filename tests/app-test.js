a7560ff456c8fc98c6724af475882b4a4e6320af
halitbatur <halitfuatbatur@gmail.com>
2021-10-21 07:36:08 +0300

    add tests for post request

--- tests/app-test.js
+++ tests/app-test.js
@@ -17,46 +17,46 @@ describe('GET /', () => {
     });
 });
 
-// describe("POST /memes", () => {
-//     it("respond with 201 created",(done) => {
-//       const meme = {
-//         name: "A new post",
-//         imgSource: "Lorem ipsum lorem ipsum",
-//         genre: ["blog", "lifestyle"],
-//         id:"4"
-//       };
+describe("POST /memes", () => {
+    it("respond with 201 created",(done) => {
+      const meme = {
+        name: "A new post",
+        imgSource: "Lorem ipsum lorem ipsum",
+        genre: ["blog", "lifestyle"],
+        id:"4"
+      };
   
-//       request
-//         .post("/memes")
-//         .set("Content-Type", "application/json")
-//         .send(meme)
-//         .expect("Content-Type", /json/)
-//         .expect(201, (err, res) => {
-//           if (err) return done(err);
-//           // use exact insitead of to be a 
-//           expect(res.body.name).to.be.a("string");
-//           expect(res.body.imgSource).to.be.a("string");
-//           expect(res.body.genre).to.be.a("array");
-//           done();
-//         });
-//     });
+      request
+        .post("/memes")
+        .set("Content-Type", "application/json")
+        .send(meme)
+        .expect("Content-Type", /json/)
+        .expect(201, (err, res) => {
+          if (err) return done(err);
+          // use exact insitead of to be a 
+          expect(res.body.name).to.be.a("string");
+          expect(res.body.imgSource).to.be.a("string");
+          expect(res.body.genre).to.be.a("array");
+          done();
+        });
+    });
   
-//     it('respond with 400 not created when id is not passed',(done) => {
-//       const meme = {
-//         name: "A new post",
-//         imgSource: "Lorem ipsum lorem ipsum",
-//         genre: ["blog", "lifestyle"],
-//       };
+    it('respond with 400 not created when id is not passed',(done) => {
+      const meme = {
+        name: "A new post",
+        imgSource: "Lorem ipsum lorem ipsum",
+        genre: ["blog", "lifestyle"],
+      };
   
-//       request
-//           .post('/memes')
-//           .send(meme)
-//           .set('Accept', 'application/json')
-//           .expect('Content-Type', /json/)
-//           .expect(400)
-//           .expect('"meme not created"')
-//           .end((err) => {
-//               if (err) return done(err);
-//               done();
-//           });
-//   });
+      request
+          .post('/memes')
+          .send(meme)
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(400)
+          .expect('"meme not created"')
+          .end((err) => {
+              if (err) return done(err);
+              done();
+          });
+  });
